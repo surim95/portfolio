657c97596ea7bf8f883c2a69fd1a1cee4faf1e70
surim95 <cheasulim2731@gmail.com>
2025-07-17 21:59:40 +0900

    포트폴리오

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -6,7 +6,9 @@ const wrap = new Swiper("#total_wrap", {
     slideChange: function () {
       const i = this.activeIndex;
       gnbLi.forEach((gnbRemove) => gnbRemove.classList.remove("active"));
-      gnbLi[i].classList.add("active");
+      if (gnbLi[i]) {
+        gnbLi[i].classList.add("active");
+      }
     },
   },
 });
@@ -20,4 +22,22 @@ const webDesign = new Swiper(".web_slide", {
     prevEl: ".web_slide .swiper-button-prev",
     nextEl: ".web_slide .swiper-button-next",
   },
+});
+
+/* graphic design swiper */
+const bannerSlide = new Swiper(".banner_container", {
+  slidesPerView: "auto",
+  loop: true,
+});
+
+const posterSlide = new Swiper(".poster_container", {
+  slidesPerView: "auto",
+  loop: true,
+});
+
+/* longPoster swiper */
+const longPosterSlide = new Swiper(".long_design", {
+  /* autoplay: { delay: 2000 }, */
+  loop: true,
+  slidesPerView: 1,
 });
